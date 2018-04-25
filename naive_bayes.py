d2cb0404b7a665a9d6d11cc8aa9a5f50014c361f
miraaitsaada <mira.ait.94@gmail.com>
2018-04-25 23:11:38 +0200

    ffdsf

--- naive_bayes.py
+++ naive_bayes.py
@@ -56,7 +56,7 @@ if __name__ == "__main__" :
     file.write("\n\n\n\n********************************************************************************\n")
     file.write(">>> Date time : " + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "\n")
     file.write(">>> Part : "+ str(part) + " \n")
-    
+    file.write(">>> Part : "+ str(part) + " \n")
     
     ###########################################################################
     #########                 Training and Test Set                   #########
@@ -68,8 +68,8 @@ if __name__ == "__main__" :
     training = training_idf[0]
     idf = training_idf[1]
     
-    test_file = "data/test_clean"+ str(part) + ".csv"
-    test = test_set(test_file, idf)
+#    test_file = "data/test_clean"+ str(part) + ".csv"
+#    test = test_set(test_file, idf)
     
     print("\nDone : Tf-IDF training and test sets")
     
@@ -91,22 +91,22 @@ if __name__ == "__main__" :
     ###########################################################################
     #########                     Model Testing                       #########
     
-    print("\n=================== Testing =================== \n")
-    
-    
-    #Bayes
-    predictions_bayes = model_bayes.predict(test)
-    num_pos_bayes = predictions_bayes.countByValue()[1.0]
-    num_neg_bayes = predictions_bayes.countByValue()[0.0]
-    
-    print("== PREDICTION BAYES : ==\n")
-    print("- Positive : " , num_pos_bayes)
-    print("- Negative : " , num_neg_bayes)
-    
-    file.write("\n\n" + "======================== BAYES ======================== " + "\n\n")
-    file.write("- Positive : " + str(num_pos_bayes) + "\n")
-    file.write("- Negative : " + str(num_neg_bayes) + "\n")
-    
+#    print("\n=================== Testing =================== \n")
+#    
+#    
+#    #Bayes
+#    predictions_bayes = model_bayes.predict(test)
+#    num_pos_bayes = predictions_bayes.countByValue()[1.0]
+#    num_neg_bayes = predictions_bayes.countByValue()[0.0]
+#    
+#    print("== PREDICTION BAYES : ==\n")
+#    print("- Positive : " , num_pos_bayes)
+#    print("- Negative : " , num_neg_bayes)
+#    
+#    file.write("\n\n" + "======================== BAYES ======================== " + "\n\n")
+#    file.write("- Positive : " + str(num_pos_bayes) + "\n")
+#    file.write("- Negative : " + str(num_neg_bayes) + "\n")
+#    
     
     
     ###########################################################################
@@ -115,19 +115,19 @@ if __name__ == "__main__" :
     
     print("\n========= Test on Brexit labeled data ========= ")
     
-    text_negative = sc.textFile("data/brexit_negatif_clean.csv")
-    text_positive = sc.textFile("data/brexit_positif_clean.csv")
+    text_negative_brexit = sc.textFile("data/brexit_negatif_clean.csv")
+    text_positive_brexit = sc.textFile("data/brexit_positif_clean.csv")
 
-    test_text = text_negative.union(text_positive)
-    test_tlabels = text_negative.map(lambda x: 0.0).union(text_positive.map(lambda x: 1.0))
+    test_text_brexit = text_negative_brexit.union(text_positive_brexit)
+    test_tlabels_brexit = text_negative_brexit.map(lambda x: 0.0).union(text_positive_brexit.map(lambda x: 1.0))
     
-    tf_test = HashingTF(numFeatures=10000).transform(test_text.map(lambda x : x))
+    tf_test_brexit = HashingTF(numFeatures=10000).transform(test_text_brexit.map(lambda x : x))
     
-    tfidf_test = idf.transform(tf_test)
+    tfidf_test_brexit = idf.transform(tf_test_brexit)
 #    
 #    prediction and evaluation
 #    bayes
-    labeled_prediction_bayes = test_tlabels.zip(model_bayes.predict(tfidf_test)).map(lambda x: {"actual": x[0], "predicted": x[1]})
+    labeled_prediction_bayes = test_tlabels_brexit.zip(model_bayes.predict(tfidf_test_brexit)).map(lambda x: {"actual": x[0], "predicted": x[1]})
     accuracy_bayes = 1.0 * labeled_prediction_bayes.filter(lambda doc: doc["actual"] == doc['predicted']).count() / labeled_prediction_bayes.count()
 
     
