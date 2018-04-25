c9697e9f5212125d19d18201827ab6ca0e5a68a7
miraaitsaada <mira.ait.94@gmail.com>
2018-04-25 19:15:49 +0200

    num_features mllib

--- learning_models_MLlib.py
+++ learning_models_MLlib.py
@@ -21,7 +21,7 @@ def training_set(pos_file, neg_file):
     train_text = text_negative.union(text_positive)
     train_labels = text_negative.map(lambda x: 0.0).union(text_positive.map(lambda x: 1.0))
     
-    tf = HashingTF().transform(train_text.map(lambda x : x))
+    tf = HashingTF(numFeatures=10000).transform(train_text.map(lambda x : x))
     idf = IDF().fit(tf)
     train_tfidf = idf.transform(tf)
     
@@ -116,7 +116,7 @@ if __name__ == "__main__" :
     test_text = text_negative.union(text_positive)
     test_tlabels = text_negative.map(lambda x: 0.0).union(text_positive.map(lambda x: 1.0))
     
-    tf_test = HashingTF().transform(test_text.map(lambda x : x))
+    tf_test = HashingTF(numFeatures=10000).transform(test_text.map(lambda x : x))
     
     tfidf_test = idf.transform(tf_test)
     
