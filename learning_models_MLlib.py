f11caf5651f05e40464771606222280a703b154a
miraaitsaada <mira.ait.94@gmail.com>
2018-04-25 20:11:36 +0200

    dsds

--- learning_models_MLlib.py
+++ learning_models_MLlib.py
@@ -9,7 +9,7 @@ Created on Tue Apr 24 17:18:02 2018
 from pyspark import SparkContext, SparkConf
 from pyspark.mllib.feature import HashingTF, IDF
 from pyspark.mllib.regression import LabeledPoint
-from pyspark.mllib.classification import NaiveBayes
+#from pyspark.mllib.classification import NaiveBayes
 from pyspark.mllib.tree import DecisionTree
 from datetime import datetime
 
@@ -76,63 +76,63 @@ if __name__ == "__main__" :
     #########                     Model Training                      #########
     
     
-    print("\n======================================================= ")
-    print("======================== BAYES ======================== ")
-    print("=======================================================\n")
-    
-    print("\n================== Training ===================\n")
-    
-    model_bayes = NaiveBayes.train(training)
-    print("Done : Bayes training ")
-
-
-    ###########################################################################
-    #########                     Model Testing                       #########
-    
-    print("\n=================== Testing =================== \n")
-    
-    
-    #Bayes
-    predictions_bayes = model_bayes.predict(test)
-    num_pos_bayes = predictions_bayes.countByValue()[1.0]
-    num_neg_bayes = predictions_bayes.countByValue()[0.0]
-    
-    print("== PREDICTION BAYES : ==\n")
-    print("- Positive : " , num_pos_bayes)
-    print("- Negative : " , num_neg_bayes)
-    
-    file.write("\n\n" + "======================== BAYES ======================== " + "\n\n")
-    file.write("- Positive : " + str(num_pos_bayes) + "\n")
-    file.write("- Negative : " + str(num_neg_bayes) + "\n")
-    
-    
-    
+#    print("\n======================================================= ")
+#    print("======================== BAYES ======================== ")
+#    print("=======================================================\n")
+#    
+#    print("\n================== Training ===================\n")
+#    
+#    model_bayes = NaiveBayes.train(training)
+#    print("Done : Bayes training ")
+#
+#
+#    ###########################################################################
+#    #########                     Model Testing                       #########
+#    
+#    print("\n=================== Testing =================== \n")
+#    
+#    
+#    #Bayes
+#    predictions_bayes = model_bayes.predict(test)
+#    num_pos_bayes = predictions_bayes.countByValue()[1.0]
+#    num_neg_bayes = predictions_bayes.countByValue()[0.0]
+#    
+#    print("== PREDICTION BAYES : ==\n")
+#    print("- Positive : " , num_pos_bayes)
+#    print("- Negative : " , num_neg_bayes)
+#    
+#    file.write("\n\n" + "======================== BAYES ======================== " + "\n\n")
+#    file.write("- Positive : " + str(num_pos_bayes) + "\n")
+#    file.write("- Negative : " + str(num_neg_bayes) + "\n")
+#    
+#    
+#    
 #    ###########################################################################
 #    #########           Testing on Brexit Labeled Data                #########
 #    
 #    
 #    print("\n========= Test on Brexit labeled data ========= ")
     
-    text_negative = sc.textFile("data/brexit_negatif_clean.csv")
-    text_positive = sc.textFile("data/brexit_positif_clean.csv")
-
-    test_text = text_negative.union(text_positive)
-    test_tlabels = text_negative.map(lambda x: 0.0).union(text_positive.map(lambda x: 1.0))
+#    text_negative = sc.textFile("data/brexit_negatif_clean.csv")
+#    text_positive = sc.textFile("data/brexit_positif_clean.csv")
+#
+#    test_text = text_negative.union(text_positive)
+#    test_tlabels = text_negative.map(lambda x: 0.0).union(text_positive.map(lambda x: 1.0))
+#    
+#    tf_test = HashingTF(numFeatures=10000).transform(test_text.map(lambda x : x))
+#    
+#    tfidf_test = idf.transform(tf_test)
     
-    tf_test = HashingTF(numFeatures=10000).transform(test_text.map(lambda x : x))
-    
-    tfidf_test = idf.transform(tf_test)
-    
-#    prediction and evaluation
-#    bayes
-    labeled_prediction_bayes = test_tlabels.zip(model_bayes.predict(tfidf_test)).map(lambda x: {"actual": x[0], "predicted": x[1]})
-    accuracy_bayes = 1.0 * labeled_prediction_bayes.filter(lambda doc: doc["actual"] == doc['predicted']).count() / labeled_prediction_bayes.count()
-
-    
-    print('\n== ACCURACY BAYES : ', accuracy_bayes , '==')
-    
-    file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
-    file.write('\n-> ACCURACY BAYES : ' + str(accuracy_bayes) + '\n')
+    #prediction and evaluation
+    #bayes
+#    labeled_prediction_bayes = test_tlabels.zip(model_bayes.predict(tfidf_test)).map(lambda x: {"actual": x[0], "predicted": x[1]})
+#    accuracy_bayes = 1.0 * labeled_prediction_bayes.filter(lambda doc: doc["actual"] == doc['predicted']).count() / labeled_prediction_bayes.count()
+#
+#    
+#    print('\n== ACCURACY BAYES : ', accuracy_bayes , '==')
+#    
+#    file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
+#    file.write('\n-> ACCURACY BAYES : ' + str(accuracy_bayes) + '\n')
     
     
     
@@ -165,17 +165,17 @@ if __name__ == "__main__" :
     file.write("- Negative : " + str(num_neg_entropy) + "\n")
     
     
-    print("\n========= Test on Brexit labeled data ========= ")
-    
-    #decision tree entropy
-    labeled_prediction_entropy = test_tlabels.zip(model_decision_tree_entropy.predict(tfidf_test)).map(lambda x: {"actual": x[0], "predicted": x[1]})
-    accuracy_entropy = 1.0 * labeled_prediction_entropy.filter(lambda doc: doc["actual"] == doc['predicted']).count() / labeled_prediction_entropy.count()
-    
-    
-    print('\n== ACCURACY DT ENTROPY : ', accuracy_entropy , '==')
-    
-    file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
-    file.write('\n-> ACCURACY DT ENTROPY : ' + str(accuracy_entropy) + '\n')
+#    print("\n========= Test on Brexit labeled data ========= ")
+#    
+#    #decision tree entropy
+#    labeled_prediction_entropy = test_tlabels.zip(model_decision_tree_entropy.predict(tfidf_test)).map(lambda x: {"actual": x[0], "predicted": x[1]})
+#    accuracy_entropy = 1.0 * labeled_prediction_entropy.filter(lambda doc: doc["actual"] == doc['predicted']).count() / labeled_prediction_entropy.count()
+#    
+#    
+#    print('\n== ACCURACY DT ENTROPY : ', accuracy_entropy , '==')
+#    
+#    file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
+#    file.write('\n-> ACCURACY DT ENTROPY : ' + str(accuracy_entropy) + '\n')
     
     
     
@@ -207,17 +207,17 @@ if __name__ == "__main__" :
     file.write("- Negative : " + str(num_neg_gini) + "\n")
     
     
-    print("\n========= Test on Brexit labeled data ========= ")
-    
-    #decision tree gini
-    labeled_prediction_gini = test_tlabels.zip(model_decision_tree_gini.predict(tfidf_test)).map(lambda x: {"actual": x[0], "predicted": x[1]})
-    accuracy_gini = 1.0 * labeled_prediction_gini.filter(lambda doc: doc["actual"] == doc['predicted']).count() / labeled_prediction_gini.count()
-    
-    print('\n== ACCURACY DT GINI : ', accuracy_gini , '==')
-    
-    file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
-    file.write('\n-> ACCURACY DT GINI : ' + str(accuracy_gini) + '\n')
-    
+#    print("\n========= Test on Brexit labeled data ========= ")
+#    
+#    #decision tree gini
+#    labeled_prediction_gini = test_tlabels.zip(model_decision_tree_gini.predict(tfidf_test)).map(lambda x: {"actual": x[0], "predicted": x[1]})
+#    accuracy_gini = 1.0 * labeled_prediction_gini.filter(lambda doc: doc["actual"] == doc['predicted']).count() / labeled_prediction_gini.count()
+#    
+#    print('\n== ACCURACY DT GINI : ', accuracy_gini , '==')
+#    
+#    file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
+#    file.write('\n-> ACCURACY DT GINI : ' + str(accuracy_gini) + '\n')
+#    
     file.close()
     
     
