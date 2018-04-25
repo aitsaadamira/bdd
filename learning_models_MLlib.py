7d63916178e2fad0d9efc2a524acfbe2061d44c0
miraaitsaada <mira.ait.94@gmail.com>
2018-04-25 14:44:39 +0200

    big training and tests

--- learning_models_MLlib.py
+++ learning_models_MLlib.py
@@ -11,7 +11,7 @@ from pyspark.mllib.feature import HashingTF, IDF
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.classification import NaiveBayes
 from pyspark.mllib.tree import DecisionTree
-import sys
+from datetime import datetime
 
 
 def training_set(pos_file, neg_file):
@@ -33,20 +33,15 @@ def test_set(test_file, idf):
     test_text = sc.textFile(test_file)
     
     tf_test = HashingTF().transform(test_text.map(lambda x : x))
-#    idf_test = IDF().fit(tf_test)
     tfidf_test = idf.transform(tf_test)
     return tfidf_test
 
 
 if __name__ == "__main__" :
 
-#    if (len(sys.argv)) == 3 :
-#        print("\nARGS : training_positive, training_negative, test_set")
-#        sys.exit()
-
     ###########################################################################
     #########                      Spark Context                      #########
-
+    
     conf = SparkConf().\
     setAppName('sentiment-analysis').\
     setMaster('local[*]')
@@ -55,104 +50,67 @@ if __name__ == "__main__" :
     
     
     ###########################################################################
-    #########                      Training Set                       #########
+    #########                 Training and Test Set                   #########
     
-#    "/home/mira/TAF/projet_BDD/code_BDD/train_positif.csv"
-#    "/home/mira/TAF/projet_BDD/code_BDD/train_negatif.csv"
-#    "/home/mira/TAF/projet_BDD/code_BDD/test.csv"
-    
-    pos_file = "data/training_positif.csv" #sys.argv[1]
-    neg_file = "data/training_negatif.csv" #sys.argv[2]
+    pos_file = "data/training_positif_clean.csv"
+    neg_file = "data/training_negatif_clean.csv"
     
     training_idf = training_set(pos_file, neg_file)
     training = training_idf[0]
     idf = training_idf[1]
     
-    #nb_cols = len(training.collect()[0].features.toArray())
+    test_file = "data/test_clean.csv"
+    test = test_set(test_file, idf)
     
-    ##################
-    # SAVE TRAINING !!
-    ##################
+    print("\nDone : Tf-IDF training and test sets")
+    
     
     ###########################################################################
     #########                     Model Training                      #########
     
     
-    model_bayes = NaiveBayes.train(training)
-    model_bayes.save(sc, "bayes_model_mira.model")
-    #save bayes_model
-    model_decision_tree_entropy = DecisionTree.trainClassifier(training, categoricalFeaturesInfo={}, impurity="entropy", maxDepth=5, numClasses=2)
-    model_decision_tree_entropy.save(sc, "DT_entropy_model_mira.model")
-    #save TR_entropy_model
-    model_decision_tree_gini = DecisionTree.trainClassifier(training, categoricalFeaturesInfo={}, impurity="gini", maxDepth=5, numClasses=2)
-    model_decision_tree_gini.save(sc, "DT_gini_model_mira.model")
-    #save TR_gini_model
+    print("\n======================================================= ")
+    print("======================== BAYES ======================== ")
+    print("=======================================================\n")
     
+    print("\n================== Training ===================\n")
+    
+    model_bayes = NaiveBayes.train(training)
+    print("Done : Bayes training ")
+
 
     ###########################################################################
     #########                     Model Testing                       #########
     
-    test_file = "data/test.csv" #sys.argv[3]
-    test = test_set(test_file, idf)
-    
-    ##################
-    # SAVE TEST !!
-    ##################
-    
-    
-    model_bayes = NaiveBayes.load(sc, "bayes_model_mira.model")
-    model_decision_tree_entropy = DecisionTree.load(sc, "DT_entropy_model_mira.model")
-    model_decision_tree_gini = DecisionTree.load(sc, "DT_gini_model_mira.model")
+    print("\n=================== Testing =================== \n")
     
     file = open("resultat_MLlib.txt","a")
+    file.write("\n\n\n\n********************************************************************************\n")
+    file.write(">>> Date time : " + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "\n")
     
     #Bayes
     predictions_bayes = model_bayes.predict(test)
     num_pos_bayes = predictions_bayes.countByValue()[1.0]
     num_neg_bayes = predictions_bayes.countByValue()[0.0]
     
-    print("========== PREDICTION BAYES : ==========")
+    print("== PREDICTION BAYES : ==\n")
     print("- Positive : " , num_pos_bayes)
     print("- Negative : " , num_neg_bayes)
     
-    file.write("\n" + "====== NaiveBayes ======" + "\n")
+    file.write("\n\n" + "======================== BAYES ======================== " + "\n\n")
     file.write("- Positive : " + str(num_pos_bayes) + "\n")
     file.write("- Negative : " + str(num_neg_bayes) + "\n")
-    
-    #decision tree entropy
-    predictions_decision_tree_enptropy = model_decision_tree_entropy.predict(test)
-    num_pos_entropy = predictions_decision_tree_enptropy.countByValue()[0.0]
-    num_neg_entropy = predictions_decision_tree_enptropy.countByValue()[1.0]
-    
-    #decision tree gini
-    print("========== PREDICTION ENTROPY : ==========")
-    print("- Positive : " , num_pos_entropy)
-    print("- Negative : " , num_neg_entropy)
-    
-    file.write("\n" + "====== DecisionTree (Entropy) ======" + "\n")
-    file.write("- Positive : " + str(num_pos_entropy) + "\n")
-    file.write("- Negative : " + str(num_pos_entropy) + "\n")
-    
-    predictions_decision_tree_gini = model_decision_tree_gini.predict(test)
-    num_pos_gini = predictions_decision_tree_gini.countByValue()[0.0]
-    num_neg_gini = predictions_decision_tree_gini.countByValue()[1.0]
-    
-    print("========== PREDICTION GINI : ==========")
-    print("- Positive : " , num_pos_gini)
-    print("- Negative : " , num_neg_gini)
-    
-    
-    file.write("\n" + "====== DecisionTree (Gini) ======" + "\n")
-    file.write("- Positive : " + str(num_pos_gini) + "\n")
-    file.write("- Negative : " + str(num_pos_gini) + "\n")
     
     
     
     ###########################################################################
     #########           Testing on Brexit Labeled Data                #########
     
-    text_negative = sc.textFile("data/brexit_negatif.csv")
-    text_positive = sc.textFile("data/brexit_positif.csv")
+    
+    print("\n========= Test on Brexit labeled data ========= ")
+    
+    text_negative = sc.textFile("data/brexit_negatif_clean.csv")
+    text_positive = sc.textFile("data/brexit_positif_clean.csv")
 
     test_text = text_negative.union(text_positive)
     test_tlabels = text_negative.map(lambda x: 0.0).union(text_positive.map(lambda x: 1.0))
@@ -165,33 +123,98 @@ if __name__ == "__main__" :
     #bayes
     labeled_prediction_bayes = test_tlabels.zip(model_bayes.predict(tfidf_test)).map(lambda x: {"actual": x[0], "predicted": x[1]})
     accuracy_bayes = 1.0 * labeled_prediction_bayes.filter(lambda doc: doc["actual"] == doc['predicted']).count() / labeled_prediction_bayes.count()
+
+    
+    print('\n== ACCURACY BAYES : ', accuracy_bayes , '==')
+    
+    file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
+    file.write('\n-> ACCURACY BAYES : ' + str(accuracy_bayes) + '\n')
+    
+    
+    
+    
+    print("\n===================================================== ")
+    print("=================== DECISION TREE =================== ")
+    print("===================== (Entropy) ===================== ")
+    print("=====================================================\n")
+    
+    print("\n=================== Training ================== \n")
+    
+    
+    model_decision_tree_entropy = DecisionTree.trainClassifier(training, categoricalFeaturesInfo={}, impurity="entropy", maxDepth=5, numClasses=2)
+    print("Done : DT entropy training")
+    
+    print("\n=================== Testing =================== \n")
+    
+    #decision tree entropy
+    predictions_decision_tree_enptropy = model_decision_tree_entropy.predict(test)
+    num_pos_entropy = predictions_decision_tree_enptropy.countByValue()[1.0]
+    num_neg_entropy = predictions_decision_tree_enptropy.countByValue()[0.0]
+    
+    #decision tree gini
+    print("\n== PREDICTION ENTROPY : ==\n")
+    print("- Positive : " , num_pos_entropy)
+    print("- Negative : " , num_neg_entropy)
+    
+    file.write("\n\n" + "============== DECISION TREE (entropy) ============== " + "\n\n")
+    file.write("- Positive : " + str(num_pos_entropy) + "\n")
+    file.write("- Negative : " + str(num_neg_entropy) + "\n")
+    
+    
+    print("\n========= Test on Brexit labeled data ========= ")
     
     #decision tree entropy
     labeled_prediction_entropy = test_tlabels.zip(model_decision_tree_entropy.predict(tfidf_test)).map(lambda x: {"actual": x[0], "predicted": x[1]})
     accuracy_entropy = 1.0 * labeled_prediction_entropy.filter(lambda doc: doc["actual"] == doc['predicted']).count() / labeled_prediction_entropy.count()
     
+    
+    print('\n== ACCURACY DT ENTROPY : ', accuracy_entropy , '==')
+    
+    file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
+    file.write('\n-> ACCURACY DT ENTROPY : ' + str(accuracy_entropy) + '\n')
+    
+    
+    
+    print("\n===================================================== ")
+    print("=================== DECISION TREE =================== ")
+    print("======================= (Gini) ====================== ")
+    print("=====================================================\n")
+    
+    
+    print("\n=================== Training ================== \n")
+    
+    
+    model_decision_tree_gini = DecisionTree.trainClassifier(training, categoricalFeaturesInfo={}, impurity="gini", maxDepth=5, numClasses=2)
+    print("Done : DT gini training")
+    
+    print("\n=================== Testing =================== \n")
+    
+    predictions_decision_tree_gini = model_decision_tree_gini.predict(test)
+    num_pos_gini = predictions_decision_tree_gini.countByValue()[1.0]
+    num_neg_gini = predictions_decision_tree_gini.countByValue()[0.0]
+    
+    print("\n== PREDICTION GINI : ==\n")
+    print("- Positive : " , num_pos_gini)
+    print("- Negative : " , num_neg_gini)
+    
+    
+    file.write("\n\n" + "================ DECISION TREE (gini) =============== " + "\n\n")
+    file.write("- Positive : " + str(num_pos_gini) + "\n")
+    file.write("- Negative : " + str(num_neg_gini) + "\n")
+    
+    
+    print("\n========= Test on Brexit labeled data ========= ")
+    
     #decision tree gini
     labeled_prediction_gini = test_tlabels.zip(model_decision_tree_gini.predict(tfidf_test)).map(lambda x: {"actual": x[0], "predicted": x[1]})
     accuracy_gini = 1.0 * labeled_prediction_gini.filter(lambda doc: doc["actual"] == doc['predicted']).count() / labeled_prediction_gini.count()
     
-    print('\n===== ACCURACY BAYES : ', accuracy_bayes , '=====\n')
-    print('\n===== ACCURACY DT ENTROPY : ', accuracy_entropy , '=====\n')
-    print('\n===== ACCURACY DT GINI : ', accuracy_gini , '=====\n')
+    print('\n== ACCURACY DT GINI : ', accuracy_gini , '==')
     
-    
-    file.write("\n" + "====== Results on labeled data (Brexit) ======" + "\n")
-    file.write('\n===== ACCURACY BAYES : ' + str(accuracy_bayes) + '=====\n')
-    file.write('\n===== ACCURACY DT ENTROPY : ' + str(accuracy_entropy) + '=====\n')
-    file.write('\n===== ACCURACY DT GINI : ' + str(accuracy_gini) + '=====\n')
+    file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
+    file.write('\n-> ACCURACY DT GINI : ' + str(accuracy_gini) + '\n')
     
     file.close()
-    
-    
-    
-    
-    
-    
-    
     
     
     
--- learning_models_ml.py
+++ learning_models_ml.py
@@ -12,6 +12,7 @@ from pyspark.ml.classification import MultilayerPerceptronClassifier, LinearSVC
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import HashingTF, IDF, Tokenizer
+from datetime import datetime
 
 
 if __name__ == "__main__":
@@ -19,7 +20,7 @@ if __name__ == "__main__":
     ###########################################################################
     #########                      Spark Context                      #########
     
-    
+    conf = None
     conf = SparkConf().\
     setAppName('sentiment-analysis').\
     setMaster('local[*]')
@@ -31,13 +32,26 @@ if __name__ == "__main__":
         .appName("ml_classification") \
         .getOrCreate()
         
-        
+    
+    
     ###########################################################################
-    #########                        Training                         #########
+    #########        Tokenizing Training and Test Set                #########
+    
+        
+    #test_set
+    test_text = sc.textFile("data/test_clean.csv")
+    test_df = test_text.map(lambda x : (0,x)).toDF(["nothing" , "sentence"]) #(0,x) = bricolage
+    
+    tokenizer_test = Tokenizer(inputCol="sentence", outputCol="words")
+    wordsData_test = tokenizer_test.transform(test_df)
+        
+    df_test = wordsData_test
+    nb_features_test = df_test.rdd.map(lambda x: len(x["words"])).sum()
     
     
-    text_positive = sc.textFile("data/training_positif.csv")
-    text_negative = sc.textFile("data/training_negatif.csv")
+    #training set
+    text_positive = sc.textFile("data/training_positif_clean.csv")
+    text_negative = sc.textFile("data/training_negatif_clean.csv")
     
     pos_labels = text_positive.map(lambda x: 1.0).zip(text_positive.map(lambda x : x))
     neg_labels = text_negative.map(lambda x: 0.0).zip(text_negative.map(lambda x : x))
@@ -49,100 +63,47 @@ if __name__ == "__main__":
     
     tokenizer = Tokenizer(inputCol="sentence", outputCol="words")
     wordsData = tokenizer.transform(text_df)
+     
+    df = wordsData
+    nb_features_train = df.rdd.map(lambda x: len(x["words"])).sum()
+
     
+    #number of words
+    nb_features = max(nb_features_train , nb_features_test)
+    print("\nDone : Tokenization training and test sets")
     
-    hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=1000)
+    ###########################################################################
+    #########             TF IDF Training and Test Set                #########
+    
+    #training set
+    hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=nb_features)
     featurizedData = hashingTF.transform(wordsData)
-    # alternatively, CountVectorizer can also be used to get term frequency vectors
     
     idf = IDF(inputCol="rawFeatures", outputCol="features")
     idfModel = idf.fit(featurizedData)
     rescaledData = idfModel.transform(featurizedData)
     
-    rescaledData.select("label", "features").show()
+    #rescaledData.select("label", "features").show()
     
-    
-    #training model MLP
-    num_cols = rescaledData.select('features').collect()[0].features.size  #vocabulary size
-    layers = [num_cols , 500 , 2]
-    trainer_MLP = MultilayerPerceptronClassifier(maxIter=100, layers=layers, blockSize=128, seed=1234)
-    model_MLP = trainer_MLP.fit(rescaledData)
-    trainer_MLP.save("model_MLP.model")
-    
-    
-    #training model SVC
-    trainer_SVC = LinearSVC(maxIter=10, regParam=0.1)
-    model_linear_svc = trainer_SVC.fit(rescaledData)
-    trainer_SVC.save("model_SVC.model")
-    # Print the coefficients and intercept for linearsSVC
-    print("Coefficients: " + str(model_linear_svc.coefficients))
-    print("Intercept: " + str(model_linear_svc.intercept))
-    
-
-    ###########################################################################
-    #########                           Test                          ######### 
-    
-    test_text = sc.textFile("data/test.csv")
-    test_df = test_text.map(lambda x : x).map(lambda x : (0,x)).toDF(["nothing" , "sentence"]) #(0,x) = bricolage
-    
-    tokenizer_test = Tokenizer(inputCol="sentence", outputCol="words")
-    wordsData_test = tokenizer_test.transform(test_df)
-    
-    hashingTF_test = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=1000)
+    #test_set
+    hashingTF_test = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=nb_features)
     featurizedData_test = hashingTF_test.transform(wordsData_test)
-    # alternatively, CountVectorizer can also be used to get term frequency vectors
     
 #    idf = IDF(inputCol="rawFeatures", outputCol="features")
-#    idfModel_test = idf.fit(featurizedData_test)
+#    idfModel = idf.fit(featurizedData_test)
     rescaledData_test = idfModel.transform(featurizedData_test)
     
     rescaled_test_df = rescaledData_test.select("features")
-    rescaledData_test.select("features").show()
+    #rescaledData_test.select("features").show()
+    print("\nDone : TF-IDF training and test set")
     
     
-    model_MLP = MultilayerPerceptronClassifier.load("model_MLP.model")
-    model_MVC = LinearSVC.load("model_SVC.model")
-    
-    file = open("resultat_ml.txt","a")
-    
-    #MLP test
-    predictions_MLP = model_MLP.transform(rescaled_test_df)
-    predictions_MLP.show()
-
-    num_pos_mlp = predictions_MLP.select("prediction").rdd.map(lambda x : x["prediction"]).countByValue()[1.0]
-    num_neg_mlp = predictions_MLP.select("prediction").rdd.map(lambda x : x["prediction"]).countByValue()[0.0]
-        
-    print("========== PREDICTION MLP : ==========")
-    print("- Positive : " , num_pos_mlp)
-    print("- Negative : " , num_neg_mlp)
-    
-    file.write("\n" + "====== MLP ======" + "\n")
-    file.write("- Positive : " + str(num_pos_mlp) + "\n")
-    file.write("- Negative : " + str(num_neg_mlp) + "\n")
-    
-    #SVC test
-    predictions_svc = model_linear_svc.transform(rescaled_test_df)
-    predictions_svc.show()
-    
-    num_pos_svc = predictions_svc.select("prediction").rdd.map(lambda x : x["prediction"]).countByValue()[1.0]
-    num_neg_svc = predictions_svc.select("prediction").rdd.map(lambda x : x["prediction"]).countByValue()[0.0]
-        
-    print("========== PREDICTION : ==========")
-    print("- Positive : " , num_pos_svc)
-    print("- Negative : " , num_neg_svc)
-    
-    file.write("\n" + "====== Linear SVC ======" + "\n")
-    file.write("- Positive : " + str(num_pos_svc) + "\n")
-    file.write("- Negative : " + str(num_neg_svc) + "\n")
-
-
-
     ###########################################################################
-    #########              Test On Brexit Labeled Data                #########
+    #########       Tokenization + TF-IDF Brexit Labeled Data         #########
     
-    
-    brexit_positive = sc.textFile("data/brexit_positif.csv")
-    text_negative = sc.textFile("data/brexit_negatif.csv")
+
+    brexit_positive = sc.textFile("data/brexit_positif_clean.csv")
+    text_negative = sc.textFile("data/brexit_negatif_clean.csv")
     
     pos_labels = text_positive.map(lambda x : 1.0).zip(text_positive.map(lambda x : x))
     neg_labels = text_negative.map(lambda x : 0.0).zip(text_negative.map(lambda x : x))
@@ -151,34 +112,118 @@ if __name__ == "__main__":
     neg_df = neg_labels.toDF(["label" , "sentence"])
     test_df = pos_df.union(neg_df)
     
-    tokenizer_test = Tokenizer(inputCol="sentence", outputCol="words")
-    wordsData_test = tokenizer_test.transform(test_df)
+    tokenizer_test_brexit = Tokenizer(inputCol="sentence", outputCol="words")
+    wordsData_test_brexit = tokenizer_test_brexit.transform(test_df)
     
-    hashingTF_test = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=1000)
-    featurizedData_test = hashingTF_test.transform(wordsData_test)
+    hashingTF_test_brexit = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=nb_features)
+    featurizedData_test_brexit = hashingTF_test_brexit.transform(wordsData_test_brexit)
     
-    rescaledData_test = idfModel.transform(featurizedData_test)
+    rescaledData_test_brexit = idfModel.transform(featurizedData_test_brexit)
     
-    rescaled_test_df = rescaledData_test.select("features" , "label")
+    rescaled_test_df_brexit = rescaledData_test_brexit.select("features" , "label")
     
+    print("\nDone : Tokenization and TF-IDF")
+    
+    
+    ###########################################################################
+    #########                     Opening Result File                 #########
+    
+    file = open("resultat_ml.txt","a")
+    file.write("\n\n\n\n********************************************************************************\n")
+    file.write(">>> Date time : " + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "\n")
+    
+    
+    ###########################################################################
+    #########                    Training and Test                    #########  
+    
+    print("\n======================================================= ")
+    print("==================== NEURAL NETWORK =================== ")
+    print("=======================================================\n")
+    
+    print("\n================== Training ===================\n")
+    
+    #training model MLP
+    num_cols = rescaledData.select('features').collect()[0].features.size  #vocabulary size
+    layers = [num_cols , 500 , 2]
+    trainer_MLP = MultilayerPerceptronClassifier(maxIter=100, layers=layers, blockSize=128, seed=1234)
+    model_MLP = trainer_MLP.fit(rescaledData)
+    print("Done : Neural Network Training")
+    
+    
+    print("\n=================== Testing =================== \n")
+    #MLP test
+    predictions_MLP = model_MLP.transform(rescaled_test_df)
+    #predictions_MLP.show()
+
+    num_pos_mlp = predictions_MLP.select("prediction").rdd.map(lambda x : x["prediction"]).countByValue()[1.0]
+    num_neg_mlp = predictions_MLP.select("prediction").rdd.map(lambda x : x["prediction"]).countByValue()[0.0]
+        
+    print("== PREDICTION MLP : ==")
+    print("- Positive : " , num_pos_mlp)
+    print("- Negative : " , num_neg_mlp)
+    
+    file.write("\n\n" + "========================= MLP =========================" + "\n\n")
+    file.write("- Positive : " + str(num_pos_mlp) + "\n")
+    file.write("- Negative : " + str(num_neg_mlp) + "\n")
+    
+   
+    print("\n========= Test on Brexit labeled data =========\n ")
+
     #MLP
-    result = model_MLP.transform(rescaled_test_df)
-    predictionAndLabels = result.select("prediction", "label")
+    result_MLP = model_MLP.transform(rescaled_test_df_brexit)
+    predictionAndLabels = result_MLP.select("prediction", "label")
     evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
     accuracy_MLP = evaluator.evaluate(predictionAndLabels)
     print("Accuracy MLP = " + str(accuracy_MLP))
     
+        
+    file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
+    file.write('-> ACCURACY MLP : ' + str(accuracy_MLP) + '\n')
+    
+    
+    print("\n======================================================= ")
+    print("====================== LINEAR SVC ===================== ")
+    print("=======================================================\n")
+    
+    print("\n================== Training ===================\n")
+    
+    #training model SVC
+    trainer_SVC = LinearSVC(maxIter=10, regParam=0.1)
+    model_linear_svc = trainer_SVC.fit(rescaledData)
+    print("Done : Linear_SVC training")
+    
+    
+    print("\n=================== Testing =================== \n")
+    
+    #SVC test
+    predictions_svc = model_linear_svc.transform(rescaled_test_df)
+    #predictions_svc.show()
+    
+    num_pos_svc = predictions_svc.select("prediction").rdd.map(lambda x : x["prediction"]).countByValue()[1.0]
+    num_neg_svc = predictions_svc.select("prediction").rdd.map(lambda x : x["prediction"]).countByValue()[0.0]
+        
+    print("\n== PREDICTION SVC : ==")
+    print("- Positive : " , num_pos_svc)
+    print("- Negative : " , num_neg_svc)
+    
+    
+    file.write("\n\n" + "====================== Linear SVC =====================" + "\n\n")
+    file.write("- Positive : " + str(num_pos_svc) + "\n")
+    file.write("- Negative : " + str(num_neg_svc) + "\n")
+
+    
+    print("\n========= Test on Brexit labeled data =========\n")
+
     #svc
-    result = model_linear_svc.transform(rescaled_test_df)
-    predictionAndLabels = result.select("prediction", "label")
+    result_svc = model_linear_svc.transform(rescaled_test_df_brexit)
+    predictionAndLabels = result_svc.select("prediction", "label")
     evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
     accuracy_SVC = evaluator.evaluate(predictionAndLabels)
     print("Accuracy SVC = " + str(accuracy_SVC))
     
         
-    file.write("\n" + "====== Results on labeled data (Brexit) ======" + "\n")
-    file.write('===== ACCURACY MLP : ' + str(accuracy_MLP) + '=====\n')
-    file.write('===== ACCURACY Linear_SVC : ' + str(accuracy_SVC) + '=====\n')
+    file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
+    file.write('-> ACCURACY Linear_SVC : ' + str(accuracy_SVC) + '\n')
     
     file.close()
 
