ac3179195273cf3e2b0e537677d2d65bec04ed84
aitsaadamira <aitsaadamira@gmail.com>
2018-04-27 19:23:54 +0200

    fds

--- LinearSVC.py
+++ LinearSVC.py
@@ -12,7 +12,6 @@ from pyspark.ml.classification import LinearSVC
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import HashingTF, IDF, Tokenizer
-from datetime import datetime
 
 
 if __name__ == "__main__":
@@ -20,7 +19,6 @@ if __name__ == "__main__":
     ###########################################################################
     #########                      Spark Context                      #########
     
-    conf = None
     conf = SparkConf().\
     setAppName('sentiment-analysis').\
     setMaster('local[*]')
@@ -31,14 +29,22 @@ if __name__ == "__main__":
         .builder \
         .appName("ml_classification") \
         .getOrCreate()
-        
-    
     
     ###########################################################################
     #########        Tokenizing Training and Test Set                #########
     
         
-
+    #test_set
+    test_text = sc.textFile("data/test_clean.csv")
+    test_df = test_text.map(lambda x : (0,x)).toDF(["nothing" , "sentence"]) #(0,x) = bricolage
+    
+    tokenizer_test = Tokenizer(inputCol="sentence", outputCol="words")
+    wordsData_test = tokenizer_test.transform(test_df)
+        
+    df_test = wordsData_test
+    nb_features_test = df_test.rdd.map(lambda x: len(x["words"])).sum()
+    
+    
     #training set
     text_positive = sc.textFile("data/training_positif_clean.csv")
     text_negative = sc.textFile("data/training_negatif_clean.csv")
@@ -54,25 +60,29 @@ if __name__ == "__main__":
     tokenizer = Tokenizer(inputCol="sentence", outputCol="words")
     wordsData = tokenizer.transform(text_df)
      
+    df = wordsData
+    nb_features_train = df.rdd.map(lambda x: len(x["words"])).sum()
 
-    
-    #number of words
     nb_features = 10000
-    print("\nDone : Tokenization training set")
+    print("\nDone : Tokenization training and test sets")
     
     ###########################################################################
-    #########             TF IDF Training Set                #########
+    #########             TF IDF Training and Test Set                #########
     
     #training set
     hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=nb_features)
     featurizedData = hashingTF.transform(wordsData)
-    
     idf = IDF(inputCol="rawFeatures", outputCol="features")
     idfModel = idf.fit(featurizedData)
     rescaledData = idfModel.transform(featurizedData)
     
-    #rescaledData_test.select("features").show()
-    print("\nDone : TF-IDF training set")
+    #test_set
+    hashingTF_test = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=nb_features)
+    featurizedData_test = hashingTF_test.transform(wordsData_test)
+    rescaledData_test = idfModel.transform(featurizedData_test)
+    rescaled_test_df = rescaledData_test.select("features")
+    
+    print("\nDone : TF-IDF training and test set")
     
     
     ###########################################################################
@@ -91,12 +101,9 @@ if __name__ == "__main__":
     
     tokenizer_test_brexit = Tokenizer(inputCol="sentence", outputCol="words")
     wordsData_test_brexit = tokenizer_test_brexit.transform(test_df_brexit)
-    
     hashingTF_test_brexit = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=nb_features)
     featurizedData_test_brexit = hashingTF_test_brexit.transform(wordsData_test_brexit)
-    
     rescaledData_test_brexit = idfModel.transform(featurizedData_test_brexit)
-    
     rescaled_test_df_brexit = rescaledData_test_brexit.select("features" , "label")
     
     print("\nDone : Tokenization and TF-IDF")
@@ -105,40 +112,53 @@ if __name__ == "__main__":
     ###########################################################################
     #########                     Opening Result File                 #########
     
-    file = open("resultat_ml.txt","a")
-    file.write("\n\n\n\n********************************************************************************\n")
-    file.write(">>> Date time : " + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "\n")
-#    file.write(">>> 50/50 \n")
+    file = open("resultat_learning.txt","a")
+    file.write("\n\n\n\n*******************************************************\n")
     
     
     ###########################################################################
     #########                    Training and Test                    #########  
-    print("\n======================================================= ")
-    print("====================== LINEAR SVC ===================== ")
-    print("=======================================================\n")
-    
+        
     print("\n================== Training ===================\n")
     
-    #training model SVC
     trainer_SVC = LinearSVC(maxIter=10, regParam=0.1)
     model_linear_svc = trainer_SVC.fit(rescaledData)
     print("Done : Linear_SVC training")
-
+    
+    
+    print("\n=================== Testing =================== \n")
+    
+    predictions_svc = model_linear_svc.transform(rescaled_test_df)
+    
+    num_pos_svc = predictions_svc.select("prediction").rdd.map(lambda x : x["prediction"]).countByValue()[1.0]
+    num_neg_svc = predictions_svc.select("prediction").rdd.map(lambda x : x["prediction"]).countByValue()[0.0]
+        
+    print("\n== PREDICTION SVC : ==")
+    print("- Positive : " , num_pos_svc)
+    print("- Negative : " , num_neg_svc)
+    
+    file.write("\n\n" + "====================== Linear SVC =====================" + "\n\n")
+    file.write("- Positive : " + str(num_pos_svc) + "\n")
+    file.write("- Negative : " + str(num_neg_svc) + "\n")
     
     print("\n========= Test on Brexit labeled data =========\n")
 
-    #svc
     result_svc = model_linear_svc.transform(rescaled_test_df_brexit)
     predictionAndLabels = result_svc.select("prediction", "label")
     evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
     accuracy_SVC = evaluator.evaluate(predictionAndLabels)
     print("Accuracy SVC = " + str(accuracy_SVC))
-    
         
     file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
-    file.write('-> ACCURACY Linear_SVC : ' + str(accuracy_SVC) + '\n')
+    file.write('\n-> ACCURACY Linear_SVC : ' + str(accuracy_SVC) + '\n')
     
     file.close()
+
+
+
+
+
+
 
 
 
--- NaiveBayes.py
+++ NaiveBayes.py
@@ -10,10 +10,7 @@ from pyspark import SparkContext, SparkConf
 from pyspark.mllib.feature import HashingTF, IDF
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.classification import NaiveBayes
-from datetime import datetime
-import sys
 
-#/home/mira/TAF/projet_BDD/code_BDD/test_petit_jeu_de_donnees
 
 def training_set(pos_file, neg_file):
     text_negative = sc.textFile(neg_file)
@@ -40,8 +37,6 @@ def test_set(test_file, idf):
 
 if __name__ == "__main__" :
     
-    part = sys.argv[1]
-    
     
     ###########################################################################
     #########                      Spark Context                      #########
@@ -52,11 +47,9 @@ if __name__ == "__main__" :
     
     sc = SparkContext(conf = conf)
     
-    file = open("resultat_MLlib.txt","a")
-    file.write("\n\n\n\n********************************************************************************\n")
-    file.write(">>> Date time : " + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "\n")
-    file.write(">>> Part : "+ str(part) + " \n")
-#    file.write(">>> 50/50 \n")
+    file = open("resultat_learning.txt","a")
+    file.write("\n\n\n\n*******************************************************\n")
+    
     
     ###########################################################################
     #########                 Training and Test Set                   #########
@@ -68,21 +61,14 @@ if __name__ == "__main__" :
     training = training_idf[0]
     idf = training_idf[1]
     
-#    test_file = "data/test_clean"+ str(part) + ".csv"
-#    test = test_set(test_file, idf)
+    test_file = "data/test_clean" + ".csv"
+    test = test_set(test_file, idf)
     
     print("\nDone : Tf-IDF training and test sets")
     
     
     ###########################################################################
-    #########                     Model Training                      #########
-    
-    
-    print("\n======================================================= ")
-    print("======================== BAYES ======================== ")
-    print("=======================================================\n")
-    
-    print("\n================== Training ===================\n")
+    #########                      Model Training                     #########
     
     model_bayes = NaiveBayes.train(training)
     print("Done : Bayes training ")
@@ -91,22 +77,20 @@ if __name__ == "__main__" :
     ###########################################################################
     #########                     Model Testing                       #########
     
-#    print("\n=================== Testing =================== \n")
-#    
-#    
-#    #Bayes
-#    predictions_bayes = model_bayes.predict(test)
-#    num_pos_bayes = predictions_bayes.countByValue()[1.0]
-#    num_neg_bayes = predictions_bayes.countByValue()[0.0]
-#    
-#    print("== PREDICTION BAYES : ==\n")
-#    print("- Positive : " , num_pos_bayes)
-#    print("- Negative : " , num_neg_bayes)
-#    
-#    file.write("\n\n" + "======================== BAYES ======================== " + "\n\n")
-#    file.write("- Positive : " + str(num_pos_bayes) + "\n")
-#    file.write("- Negative : " + str(num_neg_bayes) + "\n")
-#    
+    
+    #Bayes
+    predictions_bayes = model_bayes.predict(test)
+    num_pos_bayes = predictions_bayes.countByValue()[1.0]
+    num_neg_bayes = predictions_bayes.countByValue()[0.0]
+    
+    print("\n== PREDICTION BAYES : ==\n")
+    print("- Positive : " , num_pos_bayes)
+    print("- Negative : " , num_neg_bayes)
+    
+    file.write("\n\n" + "======================== BAYES ======================== " + "\n\n")
+    file.write("- Positive : " + str(num_pos_bayes) + "\n")
+    file.write("- Negative : " + str(num_neg_bayes) + "\n")
+    
     
     
     ###########################################################################
@@ -124,9 +108,8 @@ if __name__ == "__main__" :
     tf_test_brexit = HashingTF(numFeatures=10000).transform(test_text_brexit.map(lambda x : x))
     
     tfidf_test_brexit = idf.transform(tf_test_brexit)
-#    
-#    prediction and evaluation
-#    bayes
+    
+    #prediction and evaluation
     labeled_prediction_bayes = test_tlabels_brexit.zip(model_bayes.predict(tfidf_test_brexit)).map(lambda x: {"actual": x[0], "predicted": x[1]})
     accuracy_bayes = 1.0 * labeled_prediction_bayes.filter(lambda doc: doc["actual"] == doc['predicted']).count() / labeled_prediction_bayes.count()
 
@@ -135,13 +118,6 @@ if __name__ == "__main__" :
     
     file.write("\n" + "== Results on labeled data (Brexit) ==" + "\n")
     file.write('\n-> ACCURACY BAYES : ' + str(accuracy_bayes) + '\n')
-    
-    
-
-    file.close()
-    
-    
-    
     
     
     
