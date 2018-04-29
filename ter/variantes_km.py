497e8d806aeed4f23cfc3487cc35adc73c6e7284
aitsaadamira <aitsaadamira@gmail.com>
2018-04-29 22:38:56 +0200

    fds

--- ter/variantes_km.py
+++ ter/variantes_km.py
@@ -171,32 +171,32 @@ def kmeans_exec(matrix, nb_clusters, nb_init, fun, fun_name, mat_name):
 #    clustering_list.append(km_lsa)
 #    name_list.append("lsa")
     
-    
-    km_nothing = kmeans(matrix, nb_clusters, nb_init)
-    save_res(km_nothing, fun_name + "_nothing" , mat_name, nb_clusters, nb_init)
-    
-    print("================== nothing ======================")
-    
-    km_tfidf = kmeans_tf_idf(matrix, nb_clusters, nb_init, fun)
-    save_res(km_tfidf, fun_name + "_tfidf" , mat_name , nb_clusters, nb_init)
-    
-    print("=================== tfidf =======================")
-    
-    km_norm_line = kmeans_norm_line(matrix, nb_clusters, nb_init, fun)
-    save_res(km_norm_line, fun_name + "_norm_line" , mat_name , nb_clusters, nb_init)
-    
-    print("================== norm_line =====================")
-    
-    km_norm_unit = kmeans_norm_unit(matrix, nb_clusters, nb_init, fun)
-    save_res(km_norm_unit, fun_name + "_norm_unit" , mat_name , nb_clusters, nb_init)
-    
-    print("================== norm_unit =====================")
-    
-    km_chi2 = kmeans_chi2(matrix, nb_clusters, nb_init, fun)
-    save_res(km_chi2, fun_name + "_chi2" , mat_name , nb_clusters, nb_init)
-    
-    print("===================== chi2 =======================")
-    
+    if(fun_name != "kmeans"):
+        km_nothing = kmeans(matrix, nb_clusters, nb_init)
+        save_res(km_nothing, fun_name + "_nothing" , mat_name, nb_clusters, nb_init)
+        
+        print("================== nothing ======================")
+        
+        km_tfidf = kmeans_tf_idf(matrix, nb_clusters, nb_init, fun)
+        save_res(km_tfidf, fun_name + "_tfidf" , mat_name , nb_clusters, nb_init)
+        
+        print("=================== tfidf =======================")
+        
+        km_norm_line = kmeans_norm_line(matrix, nb_clusters, nb_init, fun)
+        save_res(km_norm_line, fun_name + "_norm_line" , mat_name , nb_clusters, nb_init)
+        
+        print("================== norm_line =====================")
+        
+        km_norm_unit = kmeans_norm_unit(matrix, nb_clusters, nb_init, fun)
+        save_res(km_norm_unit, fun_name + "_norm_unit" , mat_name , nb_clusters, nb_init)
+        
+        print("================== norm_unit =====================")
+        
+        km_chi2 = kmeans_chi2(matrix, nb_clusters, nb_init, fun)
+        save_res(km_chi2, fun_name + "_chi2" , mat_name , nb_clusters, nb_init)
+        
+        print("===================== chi2 =======================")
+        
     clustering_list = [km_nothing, km_tfidf, km_norm_line, km_norm_unit, km_chi2]
     name_list = ["/" , "tfidf" , "norm_line" , "norm_unit" , "chi2"]
     
@@ -208,7 +208,7 @@ def kmeans_exec(matrix, nb_clusters, nb_init, fun, fun_name, mat_name):
     
     print("====================== LSA =======================")
 
-    compare_clustering(clustering_list, name_list = name_list, fun_name = fun_name, mat_name = mat_name)
+#    compare_clustering(clustering_list, name_list = name_list, fun_name = fun_name, mat_name = mat_name)
     
     
 
